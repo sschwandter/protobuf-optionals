096a71475917fc3f1d3eda5f6ef3dfa0057eee0e
Stefan Schwandter <s.schwandter@me.com>
2019-01-14 21:51:47 +0100

    Serialize and deserialize

--- src/test/java/org/shockfrosted/protobuf_optionals/MainTest.java
+++ src/test/java/org/shockfrosted/protobuf_optionals/MainTest.java
@@ -1,50 +1,62 @@
 package org.shockfrosted.protobuf_optionals;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.junit.Test;
 
 public class MainTest {
 
     @Test
-    public void subMessageSet() {
-        Testproto.MyMessage message = Testproto.MyMessage
+    public void subMessageSet() throws InvalidProtocolBufferException {
+        byte[] messageArray = Testproto.MyMessage
                 .newBuilder()
                 .setMessage(Testproto.MySubMessage
                         .newBuilder()
                         .setMyInt(2)
                         .build())
-                .build();
+                .build()
+                .toByteArray();
+
+        Testproto.MyMessage message = Testproto.MyMessage.parseFrom(messageArray);
 
         System.out.println("Submessage set - message.hasMessage:" + (message.hasMessage()) );
     }
 
     @Test
-    public void subMessageSetToDefault() {
-        Testproto.MyMessage message = Testproto.MyMessage
+    public void subMessageSetToDefault() throws InvalidProtocolBufferException {
+        byte[] messageArray = Testproto.MyMessage
                 .newBuilder()
                 .setMessage(Testproto.MySubMessage.getDefaultInstance())
-                .build();
+                .build().toByteArray();
+
+        Testproto.MyMessage message = Testproto.MyMessage.parseFrom(messageArray);
 
         System.out.println("Submessage set to default instance - message.hasMessage:" + (message.hasMessage()) );
     }
 
     @Test
-    public void subMessageSetToDefaultMemberInstance() {
-        Testproto.MyMessage message = Testproto.MyMessage
+    public void subMessageSetToDefaultMemberInstance() throws InvalidProtocolBufferException {
+        byte[] messageArray = Testproto.MyMessage
                 .newBuilder()
                 .setMessage(Testproto.MySubMessage
                         .newBuilder()
                         .setMyInt(0)
                         .build())
-                .build();
+                .build()
+                .toByteArray();
+
+        Testproto.MyMessage message = Testproto.MyMessage.parseFrom(messageArray);
 
         System.out.println("Int in Submessage set to default value - message.hasMessage:" + (message.hasMessage()) );
     }
 
     @Test
-    public void subMessageMissing() {
-        Testproto.MyMessage message = Testproto.MyMessage
+    public void subMessageMissing() throws InvalidProtocolBufferException {
+        byte[] messageArray = Testproto.MyMessage
                 .newBuilder()
-                .build();
+                .build()
+                .toByteArray();
+
+        Testproto.MyMessage message = Testproto.MyMessage.parseFrom(messageArray);
 
         System.out.println("Submessage missing - message.hasMessage:" + (message.hasMessage()));
     }
